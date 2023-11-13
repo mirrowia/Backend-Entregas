bf42f1a464f2a3569fccfa2da42a3316743bd3fc
MirrowIA <andresisella@gmail.com>
2023-11-13 19:41:39 -0300

    Se implementó el loggin en session

--- src/controllers/sessions.js
+++ src/controllers/sessions.js
@@ -1,6 +1,7 @@
 const sessionService = require("../services/session")
 const { createHash, decodedToken } = require("../utils");
 const passport = require("passport");
+const logger = require("../config/logger")
 
 async function renderLogin(req, res) {
   return res.render('login');
@@ -9,6 +10,11 @@ async function login(req, res, next) {
     passport.authenticate("login", (err, token) => {
         if (err) return res.status(500).send(err.message);
         if (!token)return res.status(400).send("Invalid credentials");
+
+        // GET EMAIL
+        const email = decodedToken(token).email
+        logger.info(`El usuario ${email} acaba de iniciar sesion`)
+
         // USER IS AUTHENTICATED, SAVE TOKEN ON A COOKIE
         res.cookie('userToken', token , { maxAge: 86400000, httpOnly: true });
         return res.redirect("/api/sessions/");
@@ -18,6 +24,7 @@ async function login(req, res, next) {
 async function renderRegister(req, res) {
   const userCookie = req.cookies.user;
   if (userCookie) {
+    logger.info(`El usuario ${name} acaba de iniciar sesion`)
       const { name, lastname, email, age, cart, rol } = JSON.parse(userCookie);
       res.render("profile", { name, lastname, email, age, cart, rol });
   } else {
@@ -31,6 +38,8 @@ async function register(req, res) {
         if(user) return res.status(409).send({error: "User already exist"})
         const newUser = {name, lastname, age, email, password: createHash(password)}
         const result = await sessionService.createUser(newUser)
+
+        logger.debug(`Se acaba de registrar el usuario ${email}`)
         //result.save()
         return res.redirect("/api/sessions/login")
     } catch (error) {
@@ -39,6 +48,10 @@ async function register(req, res) {
 }
 
 async function logout(req, res) {
+
+  // GET EMAIL
+  const email = decodedToken(token).email
+  logger.info(`El usuario ${email} acaba de cerrar sesion`)
   res.clearCookie('token');
   res.redirect("./login");
 }
@@ -66,12 +79,14 @@ async function passwordChangeEmail(req, res) {
     const hashedPassword = createHash(password);
     user.password = hashedPassword;
     sessionService.updateUser(user)
+
+    logger.info("El usuario cambió la clave")
     return res
     .status(200)
     .send({ status: "ok", message: "Clave cambiada correctamente." });
 
   } catch (error) {
-    console.log(error);
+    logger.error(error)
   }
 }
 
@@ -93,7 +108,7 @@ try {
   .send({ status: "ok", message: "Clave cambiada correctamente." });
 
 } catch (error) {
-  console.log(error);
+  logger.error(error)
 }
 }
 
@@ -105,6 +120,10 @@ async function githubCallback(req, res, next) {
   passport.authenticate('github', async (err, token) => {
       if (err) return res.status(500).send('Error durante la autenticación con GitHub');
       if (!token) return res.status(401).send('Error durante la autenticación con GitHub');
+
+      // GET EMAIL
+      const email = decodedToken(token).email
+      logger.info(`El usuario ${email} acaba de iniciar sesion`)
 
       // Almacena la información del usuario en la cookie
       res.cookie("userToken", token,  { maxAge: 86400000, httpOnly: true });
